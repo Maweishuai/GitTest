1517356b195ecc582f3ae85785ae2a5191263caf
Maweishuai <1377650453@qq.com>
2020-07-26 19:54:12 +0800

    第二次提交GitTest

--- test01/src/main/java/com/chaoxing/git/Testgit.java
+++ test01/src/main/java/com/chaoxing/git/Testgit.java
@@ -10,5 +10,7 @@ public class Testgit {
         System.out.println(11111111);
         System.out.println(22222222);
         System.out.println("91ma修改---"+3);
+        System.out.println("dev-"+3);
+        System.out.println("mws开发完毕-"+3);
     }
 }
