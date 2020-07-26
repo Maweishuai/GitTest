14134a696fc3fda2449cf7dd58f8541fd38f316f
Maweishuai <1377650453@qq.com>
2020-07-26 18:41:42 +0800

    第一次提交GitTest

---
+++ test01/src/main/java/com/chaoxing/git/Testgit.java
@@ -0,0 +1,14 @@
+package com.chaoxing.git;
+
+/**
+ * @author Administrator
+ * @create 2020-07-26 18:34
+ */
+public class Testgit {
+
+    public static void main(String[] args) {
+        System.out.println(11111111);
+        System.out.println(22222222);
+        System.out.println(33333333);
+    }
+}
